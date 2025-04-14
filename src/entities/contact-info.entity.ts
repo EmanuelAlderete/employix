ed6864a66b20f8ef0d685691047079af2c11a103
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-04-14 19:20:18 -0300

    feat(contact-info): :bricks: adiciona ContactInfo Entity

---
+++ src/entities/contact-info.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, JoinColumn, OneToOne, PrimaryColumn } from 'typeorm';
+import { Employee } from './employee.entity';
+
+@Entity()
+export class ContactInfo {
+  @PrimaryColumn()
+  employeeId: number;
+
+  @OneToOne(() => Employee)
+  @JoinColumn()
+  employee: Employee;
+  @Column()
+  phone: string;
+  @Column()
+  email: string;
+}
