f97c00f4ef49ba277d7968df969000d8c6082c7c
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-04-14 18:51:09 -0300

    feat: :wrench: configura banco de dados da aplicação

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite', // Configura o TypeORM para trabalhar com o SQLite
+      database: 'db.sqlite', // Cria um arquivo na raiz do projeto, para o banco de dados, chamado db.sqlite
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], // Encaneia por todos os arquivos de definição de entidade do projeto
+      synchronize: true, // Cria o banco de dados automaticamente
+      logging: true, // Adiciona Logs
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { existsSync, unlinkSync } from 'fs';
 
 async function bootstrap() {
+  const dbFile = 'db.sqlite';
+  if (existsSync(dbFile)) unlinkSync(dbFile); // Confere se o banco já existe, e se exister deleta toda vez que aplicação roda
+
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
