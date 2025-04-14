7bb398460e6aa3735e11c08ad6a35ee57d21c113
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-04-14 19:09:57 -0300

    feat(employee): :bricks: adiciona relação de usuário e gerente (many-to-one)

--- src/entities/employee.entity.ts
+++ src/entities/employee.entity.ts
@@ -1,10 +1,16 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity() // É necessário adicionar o _decorator_ do TypeORM para identificar que está sendo criada uma entity
 export class Employee {
-  @PrimaryGeneratedColumn() // Define a coluna primária com auto-increment
+  @PrimaryGeneratedColumn() // Define a coluna primária com AUTOINCREMENT
   id: number;
 
   @Column() // // Define a coluna
   name: string;
+
+  // Define a relação do UserxUser(gerente),
+  // e define que quando um gerente é excluído,
+  // seus funcionários recebem um null nessa coluna.
+  @ManyToOne(() => Employee, { onDelete: 'SET NULL' })
+  manager: Employee;
 }
