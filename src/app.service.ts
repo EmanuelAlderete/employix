74abd3573823dbb9b454d99af7de667fb21e6ee3
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-04-14 19:41:41 -0300

    feat: :seedling: adiciona o seed para as tabelas

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,56 @@
 import { Injectable } from '@nestjs/common';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { Employee } from './entities/employee.entity';
+import { ContactInfo } from './entities/contact-info.entity';
+import { Task } from './entities/task.entity';
+import { Meeting } from './entities/meeting.entity';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@InjectDataSource() private readonly dataSource: DataSource) {}
+
+  async seed() {
+    await this.dataSource.transaction(async (db) => {
+      const ceo = db.create(Employee, {
+        name: 'Mr. CEO',
+      });
+
+      await db.save(ceo);
+
+      const contactInfo = db.create(ContactInfo, {
+        phone: '+555 343 3243',
+        email: 'ceo@123start.com',
+        employee: ceo,
+      });
+
+      await db.save(contactInfo);
+
+      const manager = db.create(Employee, {
+        name: 'Mr. Manager',
+        manager: ceo,
+      });
+
+      await db.save(manager);
+
+      const task1 = db.create(Task, {
+        name: 'Present to CEO',
+        assignee: manager,
+      });
+
+      const task2 = db.create(Task, {
+        name: 'Hire people',
+        assignee: manager,
+      });
+
+      await db.save([task1, task2]);
+
+      const meeting = db.create(Meeting, {
+        zoomUrl: 'zoom.com?id=123d21s2e',
+        attendees: [ceo, manager],
+      });
+
+      await db.save(meeting);
+    });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { existsSync, unlinkSync } from 'fs';
+import { AppService } from './app.service';
 
 async function bootstrap() {
   const dbFile = 'db.sqlite';
   if (existsSync(dbFile)) unlinkSync(dbFile); // Confere se o banco já existe, e se exister deleta toda vez que aplicação roda
 
   const app = await NestFactory.create(AppModule);
+
+  const appService = app.get(AppService);
+  await appService.seed();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
