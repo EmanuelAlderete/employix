7ee4d46bc46a126ddd212691f5646481b777baf0
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-04-15 09:22:07 -0300

    refactor: adiciona ValidationPipe para validação de requisições

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { existsSync, unlinkSync } from 'fs';
 import { AppService } from './app.service';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const dbFile = 'db.sqlite';
   if (existsSync(dbFile)) unlinkSync(dbFile); // Confere se o banco já existe, e se exister deleta toda vez que aplicação roda
 
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const appService = app.get(AppService);
   await appService.seed();
